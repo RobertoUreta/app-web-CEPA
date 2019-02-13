309d3220bf9d8e93e22a5b5a044b4bf3b64f1bbf
Raimundo Vasquez <rvasquez15@alumnos.utalca.cl>
2019-02-13 15:11:53 -0300

    Se arreglan lo accordion y los navbar

---
+++ src/pages/Evaluacion.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Card, Collapse } from 'react-bootstrap'
+import "../styles/styles.css"
+
+
+export class Evaluacion extends Component {
+
+    render () {
+        return (
+            null
+        )
+    }
+}
--- src/pages/General.js
+++ src/pages/General.js
@@ -4,11 +4,11 @@ import "../styles/styles.css"
 import { AdultoContacto } from './Paciente/General/AdultoContacto'
 import { DatosPersonales } from './Paciente/General/DatosPersonales'
 import { DatosSocioDemograficos } from './Paciente/General/DatosSocioDemograficos'
-import { DatosAdicionales} from './Paciente/General/DatosAdicionales'
+import { DatosAdicionales } from './Paciente/General/DatosAdicionales'
 
 export class General extends Component {
-    constructor(props, context) {
-        super(props, context);
+    constructor(props) {
+        super(props);
 
         this.state = {
             openPersonales: false,
@@ -18,28 +18,81 @@ export class General extends Component {
         };
     }
 
+    _onClickAction = (evt) => {
+        // eslint-disable-next-line default-case
+        switch (evt.currentTarget.id) {
+            case ('1'):
+                this.setState(() => ({
+                    openPersonales: !this.state.openPersonales,
+                    openAdultoContacto: false,
+                    openSocioDemo: false,
+                    openAdicionales: false
+                }));
+                break;
+
+            case('2'):
+                this.setState(() => ({
+                    openPersonales: false,
+                    openAdultoContacto: !this.state.openAdultoContacto,
+                    openSocioDemo: false,
+                    openAdicionales: false
+                }));
+                break;
+            case('3'):
+                this.setState(() => ({
+                    openPersonales: false,
+                    openAdultoContacto: false,
+                    openSocioDemo: !this.state.openSocioDemo,
+                    openAdicionales: false
+                }));
+                break;
+            case('4'):
+                this.setState(() => ({
+                    openPersonales: false,
+                    openAdultoContacto: false,
+                    openSocioDemo: false,
+                    openAdicionales: !this.state.openAdicionales
+                }));
+                break;
+
+
+            
+        }
+
+    }
     render() {
+
+
+        console.log(this.state)
 
         return (
             <div>
                 <h2>Paciente</h2>
 
                 <Card.Header
-                    onClick={() => this.setState({ openPersonales: !this.state.openPersonales })} >
+                    id="1"
+                    onClick={this._onClickAction} >
                     <span className="linkspan">
                         <strong>Datos Personales</strong>
                     </span></Card.Header>
                 <Collapse in={this.state.openPersonales}>
                     <Card.Body>
                         <DatosPersonales />
-
                     </Card.Body>
                 </Collapse>
 
                 <br></br>
 
                 <Card.Header
-                    onClick={() => this.setState({ openAdultoContacto: !this.state.openAdultoContacto })} >
+                    id="2"
+                    onClick={this._onClickAction}/*() => {
+                        this.setState(() => ({
+                            openPersonales: false,
+                            openAdultoContacto: !this.state.openAdultoContacto,
+                            openSocioDemo: false,
+                            openAdicionales: false
+                        }))
+                    }*/>
                     <span className="linkspan">
                         <strong>Adulto Contacto</strong>
                     </span></Card.Header>
@@ -53,7 +106,8 @@ export class General extends Component {
                 <br></br>
 
                 <Card.Header
-                    onClick={() => this.setState({ openSocioDemo: !this.state.openSocioDemo })} >
+                    id="3"
+                    onClick={this._onClickAction} >
                     <span className="linkspan">
                         <strong>Datos Socio-demograficos</strong>
                     </span></Card.Header>
@@ -67,13 +121,14 @@ export class General extends Component {
                 <br></br>
 
                 <Card.Header
-                    onClick={() => this.setState({ openAdicionales: !this.state.openAdicionales })} >
+                    id="4"
+                    onClick={this._onClickAction} >
                     <span className="linkspan">
                         <strong>Datos Adicionales</strong>
                     </span></Card.Header>
                 <Collapse in={this.state.openAdicionales}>
                     <Card.Body >
-                        <DatosAdicionales/>
+                        <DatosAdicionales />
                     </Card.Body>
                 </Collapse>
 
