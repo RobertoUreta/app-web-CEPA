ca44218da70c73de02dcf909062b2228d9783fb5
Unknown <roberto.ureta.14@gmail.com>
2019-02-28 18:16:25 -0300

    Insertar usuario a db

--- src/backend/usuario/usuario.js
+++ src/backend/usuario/usuario.js
@@ -3,7 +3,7 @@ let database = mysql.default
 
 
 let obtenerUsuarios = () => {
-    let query = "SELECT * FROM permiso"
+    let query = "SELECT * FROM usuario"
     
     database.ejecutarQuery(query,(err, data) => {
         if (data == null) {
@@ -16,8 +16,40 @@ let obtenerUsuarios = () => {
             console.log("funciona");
             console.log(data);
         }
-    })
+    });
 }
 
+let insertarUsuario = (obj) => {
+    let est = obj.estado;
+    let estado = est ? 1 : 0;
+    let query = `INSERT INTO usuario(id_usuario,nombre,apellido_paterno,apellido_materno,rut,genero,username,password,telefono_trabajo,telefono_movil,correo,horas_semanales,nombre_contacto_emergencia,telefono_contacto_emergencia,estado,ref_rol,ref_supervisor) VALUES(${obj.id_usuario},'${obj.nombre}','${obj.apellidoPaterno}','${obj.apellidoMaterno}','${obj.rut}','${obj.genero}','${obj.username}','${obj.password}','${obj.telefonoTrabajo}','${obj.telefonoMovil}','${obj.correo}',${obj.horasSemanales},'${obj.nombreContactoEmergencia}','${obj.telefonoContactoEmergencia}','${estado}',${obj.ref_rol},${obj.refSupervisor});`;
+    database.ejecutarQuery(query,(err, data) => {
+        if (err) {
+            console.log("error :(");
+        }
+        else {
+            console.log("realizado");
+        }
+    });
+}
+let objeto =  {
+    id_usuario: 3,
+    nombre: 'Pablos',
+    apellidoPaterno: 'ss',
+    apellidoMaterno: 'xd',
+    rut: '19275731',
+    genero: 'Masculino',
+    username: 'roberto',
+    password: '123456',
+    telefonoTrabajo: '12345',
+    telefonoMovil: '54321',
+    correo: 'roberto@gmail.com',
+    horasSemanales: 24,
+    nombreContactoEmergencia: 'raimundo',
+    telefonoContactoEmergencia: '433',
+    estado: true,
+    ref_rol: 1,
+    refSupervisor: 1 }
+insertarUsuario(objeto);
 obtenerUsuarios();
 database.cerrarConexion();
